5ffbe5a3824ec61fa5248609b440b2c955dc6a32
xutingfeng <xutingfeng@big.ac.cn>
2023-10-08 19:36:18 +0800

    add test.png

--- main_pretrain.py
+++ main_pretrain.py
@@ -186,7 +186,7 @@ def get_args_parser():
     parser.add_argument(
         "--dist_url", default="env://", help="url used to set up distributed training"
     )
-    parser.add_argument("--test_img_path", default=None)
+    parser.add_argument("--test_img_path", default="./test_img/test.png")
 
     return parser
 
